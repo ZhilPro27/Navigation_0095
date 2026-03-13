4cafd2fa8e5887e5eb49b7d14be6e46214a7b54e
ZhilPro27 <zhilalkrisna@gmail.com>
2026-03-13 19:48:45 +0700

    Menambahkan logika perhitungan total harga pada halaman Order.

--- lib/page/order_page.dart
+++ lib/page/order_page.dart
@@ -10,6 +10,22 @@ class OrderPage extends StatefulWidget {
 class _OrderPageState extends State<OrderPage> {
   @override
   Widget build(BuildContext context) {
+    final TextEditingController makananController = TextEditingController();
+    final TextEditingController minumanController = TextEditingController();
+    final TextEditingController jumlahMakananController = TextEditingController();
+    final TextEditingController jumlahMinumanController = TextEditingController();
+    int totalHarga = 0;
+    final _formKey = GlobalKey<FormState>();
+
+    void calculateTotalPrice() {
+      int jumlahMakanan = int.tryParse(jumlahMakananController.text) ?? 0;
+      int jumlahMinuman = int.tryParse(jumlahMinumanController.text) ?? 0;
+
+      setState(() {
+        totalHarga = (jumlahMakanan * 32000) + (jumlahMinuman * 5000);
+      });
+    }
+
     return const Placeholder();
   }
 }
